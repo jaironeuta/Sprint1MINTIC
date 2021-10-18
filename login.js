1866fbe72b8a61ae9bea6a2be380c756a1f76738
SteveArango <steve.arango1@gmail.com>
2021-10-18 12:03:28 -0500

    Subidos todos los archivos del sprint

---
+++ login.js
@@ -0,0 +1,13 @@
+let registros = [];
+
+function validar_captcha(rcaptcha){
+    //implementacion
+}
+
+function iniciar_sesion(){
+    //implementacion
+}
+
+module.exports.registros = registros;
+module.exports.validar_captcha = validar_captcha;
+module.exports.iniciar_sesion = iniciar_sesion;
